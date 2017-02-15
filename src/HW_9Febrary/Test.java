fc2ada9ae61fdad829d84974535a29a17087980d
Timur Shafigullin <Hometim55@gmail.com>
2017-02-16 00:30:36 +0300

    Initial commit

---
+++ src/HW_9Febrary/Test.java
@@ -0,0 +1,11 @@
+package HW_9Febrary;
+
+/**
+ * Created by Timbaev on 16.02.2017.
+ */
+public class Test {
+    
+    public static void main(String args[]) {
+        System.out.println("Test");
+    }
+}
